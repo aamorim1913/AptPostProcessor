bed7e142bcc3481401d9cc9f6c6071fa0fcb87eb
Antonio Joaquim Rosa Amorim Barbosa <Antonio.Amorim@sim.fc.ul.pt>
2022-08-18 22:22:46 +0100

    fix TRef.h

--- machine.h
+++ machine.h
@@ -2,10 +2,16 @@
 
 // Pivot coordinates only for the machine limits used in CSCAD.h and ref file CreateTRef.h  */
 const double Pivot[3]={-200.66, -193.7836, -338.3841};
+
 /* machine limits in machine coordinates xmin, xmax, ymin, ymax, zmin, zmax */
 const double MachineLimits[6]={-500,0,-400,0,-400,0};
+
 /* table center with z meassured included  3d sensor lenght */
 const double machine_table[3]={-200.66,-193.72,-388.18};
+const double machine_table_size[3]={500,350,50};
+const int machine_table_round=1;
+
+/* sensor data includes 20 mm to hold inside the cone  */
 const double sensorlenght=95;
 
 const double invalid_coord=-9999.0;
