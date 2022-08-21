9ba792f3f1c0e74b8b0688fc5047bc23869f6c65
unknown <Antonio.Amorim@sim.fc.ul.pt>
2022-08-21 20:37:47 +0100

    fix tool dl

--- clsplit/TOOLS.h
+++ clsplit/TOOLS.h
@@ -111,7 +111,7 @@ int WriteTool(int &fpause) {
 		int namestart=0;
 		for (int j = 0; j < strlen(tl[i].name); j++)  if (tl[i].name[j] == ']') namestart=j+2;
 		sprintf(sbuff, "%-4d %.16s %-+10.3lf %-+10.3lf %-+7.3lf %-+7.3lf        %d     %d     %d",
-			i,tl[i].name+namestart,tl[i].lcad,tl[i].rcad,tl[i].DL,tl[i].DR,tl[i].T1,tl[i].T2,tl[i].T3);
+			i,tl[i].name+namestart,tl[i].DL,tl[i].rcad,0.0,tl[i].DR,tl[i].T1,tl[i].T2,tl[i].T3);
 		for (int j = 0; j < 1024; j++) {
 			if (sbuff[j] == '\0') break;
 			if (sbuff[j] == '.') sbuff[j] = ',';
