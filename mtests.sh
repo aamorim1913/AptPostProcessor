ca760c5cc94935f9a8ba831fea7a4158940f60db
Antonio Joaquim Rosa Amorim Barbosa <Antonio.Amorim@sim.fc.ul.pt>
2022-11-26 17:03:55 +0000

    tests

--- mtests.sh
+++ mtests.sh
@@ -12,31 +12,74 @@ then
    exit
 fi
 
-FIND="`find . -name $2.apt `"
-FIND=${FIND#*/}
-ADIR=${FIND%/*}
+if [ "$2" = "all" ]
+then
+	ADIR="`ls -d parts-* `"
+else
+	FIND="`find . -name $2.apt `"
+	FIND=${FIND#*/}
+	ADIR=${FIND%/*}
+fi
 
 case $1 in
    run)
+	if [ "$2" = "all" ]
+	then
+		"With all use rdiff instead"
+		exit
+	fi
 	cd $ADIR && ../clsplit/clsplit $2.apt
 	cd ..
 	;;
    rdiff)
-	cd $ADIR && ../clsplit/clsplit $2.apt
-	cd ..
-	FILES="`ls tests/$ADIR/$2/`"
-	for FILE in ${FILES}
-	do
-	  diff -q machine-code/$FILE tests/$ADIR/$2/$FILE
-	done
+	if [ "$2" = "all" ]
+	then
+		mv machine-code/%FN15RUN.A machine-code/%FN15RUN.A-save
+		for A in $ADIR
+		do
+			for F in `ls $A/*.apt`
+			do
+				F=${F%.apt}
+				F=${F##*/}
+				cd $A 
+				cp ../tests/$A/$F/%FN15RUN.A ../machine-code/%FN15RUN.A
+				../clsplit/clsplit $F.apt
+				cd ..
+				FILES="`ls tests/$A/$F/`"
+				for FILE in ${FILES}
+				do
+	  				diff -q machine-code/$FILE tests/$A/$F/$FILE
+				done
+			done
+		done
+		mv machine-code/%FN15RUN.A-save machine-code/%FN15RUN.A
+	else
+		cd $ADIR && ../clsplit/clsplit $2.apt
+		cd ..
+		FILES="`ls tests/$ADIR/$2/`"
+		for FILE in ${FILES}
+		do
+	  		diff -q machine-code/$FILE tests/$ADIR/$2/$FILE
+		done
+	fi
 	;;
    rstore)
+	if [ "$2" = "all" ]
+	then
+		echo "rstore all not available"
+		exit
+	fi
 	cd $ADIR && ../clsplit/clsplit $2.apt
 	cd ..
 	mkdir -p tests/$ADIR/$2/
 	cp -f machine-code/%FN15RUN.A machine-code/[123456789]*.h  machine-code/*.scad tests/$ADIR/$2
 	;;
    diff)
+	if [ "$2" = "all" ]
+	then
+		echo "diff all not available. Use rdiff all instead"
+		exit
+	fi
 	FILES="`ls tests/$ADIR/$2/`"
 	for FILE in ${FILES}
 	do
@@ -44,9 +87,19 @@ case $1 in
 	done
 	;;
    getA)
+	if [ "$2" = "all" ]
+	then
+		echo "getA all not available"
+		exit
+	fi
 	cp -f tests/$ADIR/$2/%FN15RUN.A machine-code/
 	;;
    store)
+	if [ "$2" = "all" ]
+	then
+		echo "store all not available"
+		exit
+	fi
 	mkdir -p tests/$ADIR/$2/
 	cp -f machine-code/%FN15RUN.A machine-code/[123456789]*.h  machine-code/*.scad tests/$ADIR/$2
 	;;
@@ -54,10 +107,21 @@ case $1 in
 	if [ "$2" = "all" ]
 	then
 		rm -rf tests
+	else
+		rm -rf tests/$ADIR/$2/
 	fi
 	;;
    list)
-	ls -rt tests/$ADIR/$2
+	if [ "$2" = "all" ]
+	then
+		for A in ${ADIR}
+		do
+			echo "In $A"
+			ls -rt tests/$A/
+		done
+	else
+		ls -rt tests/$ADIR/$2
+ 	fi
 	;;
    *)
 	echo "Invalid option"
