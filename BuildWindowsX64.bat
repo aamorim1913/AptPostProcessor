92c0f68b383861d53af21c03e33dabe3e186b9c0
unknown <Antonio.Amorim@sim.fc.ul.pt>
2023-07-30 22:25:49 +0100

    BuildWindowsX64.bat

--- BuildWindowsX64.bat
+++ BuildWindowsX64.bat
@@ -1,1 +1,2 @@
 "C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64
+"C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\VsDevCmd.bat" -arch=amd64
