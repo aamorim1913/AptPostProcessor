c71b3e3b662f2efef01f90418753d6debd93603d
aamorim1913 <aamorim@sim.ul.pt>
2025-06-13 22:19:32 +0100

    skiptool

--- skiptool/skiptool.cpp
+++ skiptool/skiptool.cpp
@@ -4,6 +4,7 @@
 
 #define MAXLINE 1024
 #define DMUDIR "../machine-code/%d.h"
+#define DMUDIRS "../machine-code/%dS.h"
 
 int main(int argc, char* argv[]) {
 
@@ -12,39 +13,43 @@ int main(int argc, char* argv[]) {
     char prevline[MAXLINE];
     char *found;
     FILE *OUT, *IN;
-    int n1=0,n2=0,copy,definedtool;
+    int n1=0,n2=0,n3=0,n4=0,copy,definedtool,wln=1;
 
     if (argc < 2) {
-        printf("Usage: %s n1 <n2>\n", argv[0]);
+        printf("Usage: %s n1 <n2> <n3> <n4>\n", argv[0]);
         return 1;
     }
     n1=atoi(argv[1]);
-    if (argc == 3) n2 = atoi(argv[2]);
+    if (argc > 2) n2 = atoi(argv[2]);
+	if (argc > 3) n3 = atoi(argv[3]);
+	if (argc > 4) n4 = atoi(argv[4]);
 
     for (int i = 11; i < 32; i++) {
 		snprintf(filename, MAXLINE, DMUDIR, i);
 		if ( (IN=fopen(filename, "r")) == NULL) continue;
 		printf("Processing file %s into %i.h\n",filename,i+100);
 
-		snprintf(filename, MAXLINE, DMUDIR, i+100);
+		snprintf(filename, MAXLINE, DMUDIRS, i);
 		OUT=fopen(filename, "w");
 		copy=1;
 
+		fgets(prevline, MAXLINE, IN); 
 		fgets(prevline, MAXLINE, IN);
-		fgets(prevline, MAXLINE, IN);
-		fprintf(OUT,"1 BEGIN PGM %d MM\n",i+100);
-
+		fprintf(OUT,"%d BEGIN PGM %dS MM\n",wln,i); ++wln;
+    
  		while (fgets(codeline, MAXLINE, IN)) {
 			if ((found=strstr(codeline,"TOOL DEF")) != NULL) {
 				copy=1;
 				definedtool = atoi(found + strlen("TOOL DEF"));
-				if ((definedtool == n1) || (definedtool == n2)) copy=0;
+				if ((definedtool == n1) || (definedtool == n2) || (definedtool == n3) || (definedtool == n4)) copy=0;
 				printf("In %d.h Found DEF TOOL n=%d copy %d\n",i,definedtool,copy);
 			}
-        		if (copy == 1) fprintf(OUT,"%s", prevline); // Print each line
-			strcpy(prevline,codeline);
+        		if (copy == 1) {
+					 fprintf(OUT,"%d %s", wln, prevline); ++wln; // Print each line
+				}
+			strcpy(prevline,strstr(codeline," "));
     		}
-		fprintf(OUT,"1 END PGM %d MM\n",i+100);
+		fprintf(OUT,"%d END PGM %dS MM\n",wln, i);
 
  		fclose(IN);
 		fclose(OUT);
